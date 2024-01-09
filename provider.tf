a2ab39a62d779fe5532b6c402f302434e871aaa8
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2024-01-09 16:28:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
-provider "genesyscloud" {
+/*provider "genesyscloud" {
   oauthclient_id     = "cb33ef07-6fda-4f27-b940-18d31105cfc5"
   oauthclient_secret = "ocdm80omHZquCvdvGvrilS9V-h4ncrL6Q8B0xDxuZ0g"
   aws_region         = "eu-west-1"
 }
+*/
