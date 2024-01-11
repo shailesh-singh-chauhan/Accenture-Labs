a2b28f0b1a0d6b329c0ae1d9167ed154438ab9f7
Singh <shailesh.h.singh@accenture.com>
2024-01-11 17:24:27 +0530

    Blank

--- main_var.tf
+++ main_var.tf
@@ -32,8 +32,8 @@ module "routing-wrapup-code" {
 
 
 #Module for workflow
-resource "genesyscloud_flow" "flowname" {
-  filepath = "./modules/workflows/ShaileshTest.yaml"
-  file_content_hash = filesha256("./modules/workflows/ShaileshTest.yaml") 
-}
+#resource "genesyscloud_flow" "flowname" {
+#  filepath = "./modules/workflows/ShaileshTest.yaml"
+#  file_content_hash = filesha256("./modules/workflows/ShaileshTest.yaml") 
+#}
 
