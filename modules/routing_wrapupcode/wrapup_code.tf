e8578f955ab617efc3acf5f1c96de96ae3b08a13
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2024-01-11 16:51:27 +0530

    Update wrapup_code.tf

--- modules/routing_wrapupcode/wrapup_code.tf
+++ modules/routing_wrapupcode/wrapup_code.tf
@@ -1,7 +1,8 @@
-resource "genesyscloud_routing_wrapupcode" "win1" {
+/*resource "genesyscloud_routing_wrapupcode" "win1" {
   name = "Win1"
 }
 
 resource "genesyscloud_routing_wrapupcode" "resolved" {
   name = "REESOLVED"
 }
+*/
