b46acaf7b126e0cae3e7ce9eca2a5c88dd56da4c
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2024-01-11 16:28:42 +0530

    Update datasource.tf

--- modules/routing_queue/datasource.tf
+++ modules/routing_queue/datasource.tf
@@ -10,11 +10,11 @@ data "genesyscloud_auth_division" "demo2"{
 data "genesyscloud_routing_skill" "test_skill3" {
   name = "Test Skill 3"
 }
-
+*/
 
 
 #WrapUpCode Data Source
 data "genesyscloud_routing_wrapupcode" "win1" {
   name = "Win1"
 }
-*/
+
