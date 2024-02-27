8bac6bde9cf51f5a0d8f12ec0c6385785c80b798
Singh <shailesh.h.singh@accenture.com>
2024-02-27 22:28:00 +0530

    First Commit

--- main_var.tf
+++ main_var.tf
@@ -2,37 +2,36 @@
 # Module for auth-division
 module "auth_division" {
   source = "./modules/auth_division"   
-}
+
 #Division IDs
 cicd_division-id = module.auth-division.cicd_division
+}
+
 
 # Module for routing-queue
 module "routing_queue" {
     source = "./modules/routing_queue"
-
-#Wrap Up Code IDs
-
-
-
 }
+
 
 
 # Module for routing-skill
 module "routing_skill" {
 	source = "./modules/routing_skill"
-}
+
 Routing Skill IDs
 cicd_skill-id = module.routing-skill.cicd_skill
 
+}
 
 #Module for routing-wrapup-code
 module "routing-wrapup-code" {
 	source = "./modules/routing_wrapupcode"
-}
+
 
 #Wrap Up Code IDs
-resolved-id= module.routing-wrapup-code.resolved
-
+resolved-id = module.routing-wrapup-code.resolved
+}
 
 
 #Module for workflow
