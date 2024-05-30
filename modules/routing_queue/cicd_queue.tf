d8a871a28fa1ceae52078cf26882e0687e91c83d
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2024-05-30 19:20:47 +0530

    Update cicd_queue.tf

--- modules/routing_queue/cicd_queue.tf
+++ modules/routing_queue/cicd_queue.tf
@@ -4,13 +4,13 @@ resource "genesyscloud_routing_queue" "cicd_queue" {
   division_id						            = var.cicd_division
   description                       = "This is a CICD Training Queue"
   acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
-  acw_timeout_ms                    = 300000
+  acw_timeout_ms                    = 400000
   skill_evaluation_method           = "BEST"
   auto_answer_only                  = true
   enable_transcription              = true
   enable_manual_assignment          = true
   calling_party_name                = "CICD"
-  calling_party_number              = "+442020202020"
+  calling_party_number              = "+919079624027"
   wrapup_codes					           	= [var.resolved,var.cicd_wrap]  
 }
 
