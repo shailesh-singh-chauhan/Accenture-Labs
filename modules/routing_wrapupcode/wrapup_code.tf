f9101e51dbfb96086d6ae8480feb87ea6840b968
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2024-06-14 18:30:35 +0530

    Update wrapup_code.tf

--- modules/routing_wrapupcode/wrapup_code.tf
+++ modules/routing_wrapupcode/wrapup_code.tf
@@ -1,8 +1,8 @@
 resource "genesyscloud_routing_wrapupcode" "cicd_wrap" {
-  name = "CICD_WRAP_UP_CODE"
+  name = "CICD_WRAP_UP_CODE_NNI"
 }
 
 resource "genesyscloud_routing_wrapupcode" "resolved" {
-  name = "RESOLVED"
+  name = "RESOLVED_WITHOUT_ERRORS"
 }
 
