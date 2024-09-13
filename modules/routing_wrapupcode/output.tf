23b2e7579f10e07d4243ae52ffc2c1344a52bd31
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2024-09-13 16:40:05 +0530

    Update output.tf

--- modules/routing_wrapupcode/output.tf
+++ modules/routing_wrapupcode/output.tf
@@ -1,7 +1,7 @@
-output "resolved" {
+/*output "resolved" {
   value = genesyscloud_routing_wrapupcode.resolved.id
 }
 
 output "cicd_wrap" {
   value = genesyscloud_routing_wrapupcode.cicd_wrap.id
-}
+}*/
