d35fafee4a27e79a12fd17cd0a53bf6dea5d7ae9
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2024-11-13 15:41:29 +0530

    Update cicd_queue.tf

--- modules/routing_queue/cicd_queue.tf
+++ modules/routing_queue/cicd_queue.tf
@@ -2,7 +2,7 @@
 resource "genesyscloud_routing_queue" "cicd_queue" {
   name                              = "CICD Queue NNI"
   division_id						            = var.cicd_division
-  description                       = "This is a CICD Training Queue"
+  description                       = "This is a CICD Training Queues"
   acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
   acw_timeout_ms                    = 400000
   skill_evaluation_method           = "BEST"
